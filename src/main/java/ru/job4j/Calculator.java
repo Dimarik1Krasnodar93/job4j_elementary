fe49882ce92220e8fe37222ddca20f81548c5adb
Dimarik <krasnodar93@mail.ru>
2022-03-24 17:44:25 +0300

    доработка 24 03 2022 mathfunc

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -1,6 +1,17 @@
 package ru.job4j;
 
 public class Calculator {
+
+    public static int func1(int x) {
+        int y = x * x + 1;
+        return y;
+    }
+
+    public static int func2(int x) {
+        int y = 1 / x;
+        return y;
+    }
+
     public static void main(String[] args) {
 
         int one = 1;
