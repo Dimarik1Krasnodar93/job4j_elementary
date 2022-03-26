8eea57e6aab2176dc7697a4a4ecb87ba8fcc11f7
Dimarik <krasnodar93@mail.ru>
2022-03-26 12:47:58 +0300

    доработка 26 03 2022 import

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculator;
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double totalSumm(double first, double second) {
+        return sum(first, second) + difference(first, second)
+                + div(first, second) + multiply(first, second);
+    }
+
+    public static double differenceAndDiv(double first, double second) {
+        return difference(first, second)
+                + div(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+
+        System.out.println("Результат :расчета равен (разность и деление) " + differenceAndDiv(10, 20));
+
+        System.out.println("Результат :расчета равен (4 операции) " + totalSumm(10, 20));
+    }
+}
