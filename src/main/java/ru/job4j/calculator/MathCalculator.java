b12836c2986d39e520a98aa8b6c68b984c6c4163
Dimarik <krasnodar93@mail.ru>
2022-03-26 23:28:16 +0300

    доработка 26 03 2022- test - 5

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -18,6 +18,7 @@ public class MathCalculator {
     }
 
     public static void main(String[] args) {
+
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
 
         System.out.println("Результат :расчета равен (разность и деление) " + differenceAndDiv(10, 20));
