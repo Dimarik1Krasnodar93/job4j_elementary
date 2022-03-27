b78dc135f2781fd258bba0e84e15997cbba9752a
Dimarik <krasnodar93@mail.ru>
2022-03-27 10:47:55 +0300

    доработка 27-03-2022-конвертер

---
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import org.junit.Assert;
+
+public class SqAreaTest {
+
+    @Test
+    public void whenP6K2Square2() {
+        int expected = 2;
+        int p = 6;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP6K5Square1p25() {
+        int expected = 1;
+        int p = 6;
+        double k = 6;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
