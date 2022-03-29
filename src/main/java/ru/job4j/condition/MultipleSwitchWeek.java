3f283dd921a4329e3a1d37ffea62faeb40191b49
Dimarik <krasnodar93@mail.ru>
2022-03-29 20:15:40 +0300

    доработка 2903-2022- switch2

--- src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -6,7 +6,7 @@ public class MultipleSwitchWeek {
             case "Понедельник", "Monday" -> 1;
             case "Вторник", "Tuesday" -> 2;
             case "Среда", "Wednesday" -> 3;
-            case "Четверг", "Thurstay" -> 4;
+            case "Четверг", "Thursday" -> 4;
             case "Пятница", "Friday" -> 5;
             case "Суббота", "Saturday" -> 6;
             case "Воскресенье", "Sunday" -> 7;
