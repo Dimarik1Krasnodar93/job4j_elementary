9fe5bcc78e26ca136596bee0fee3c3beec0e10be
Dimarik <krasnodar93@mail.ru>
2022-03-29 22:10:26 +0300

    доработка 2903-2022- loop sum %2

---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import org.junit.Assert;
+
+public class CounterTest {
+
+    @Test
+    public void sumByEvenStart1Finish10Result30() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void sumByEvenStart3Finish10Result28() {
+        int start = 3;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 28;
+        Assert.assertEquals(expected, result);
+    }
+}
