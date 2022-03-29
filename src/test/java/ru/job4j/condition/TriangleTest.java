d8130abcdec5bcddb3c44f5fbf5cb7ffa3a81e83
Dimarik <krasnodar93@mail.ru>
2022-03-29 18:26:59 +0300

    доработка 2903-2022- triangle

--- src/test/java/ru/job4j/condition/TriangleTest.java
+++ src/test/java/ru/job4j/condition/TriangleTest.java
@@ -21,7 +21,7 @@ public class TriangleTest {
         double ab = 2.0;
         double ac = 44.0;
         double bc = 2.0;
-        boolean result = !Triangle.exist(ab, ac, bc);
-        Assert.assertTrue(result);
+        boolean result = Triangle.exist(ab, ac, bc);
+        Assert.assertFalse(result);
     }
 }
