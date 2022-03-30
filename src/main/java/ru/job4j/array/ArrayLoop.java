ee449015aff2e769552eef0ffaa9c364919b9b25
Dimarik <krasnodar93@mail.ru>
2022-03-30 20:05:49 +0300

    доработка 3003-2022- for each

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -13,5 +13,9 @@ public class ArrayLoop {
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("numbers[" + i + "] = " + numbers[i]);
         }
+
+        for (int number : numbers) {
+            System.out.println("_number: " + number);
+        }
     }
 }
