05a24fea9fabde04bd2859e707ace26a054290ef
Dimarik <krasnodar93@mail.ru>
2022-03-30 15:43:15 +0300

    доработка 3003-2022- chess-7

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -7,7 +7,7 @@ public class ChessBoard {
     }
 
     public static int way(int x1, int y1, int x2, int y2) {
-        int rsl = 0;
+        int rsl = 1;
 
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
