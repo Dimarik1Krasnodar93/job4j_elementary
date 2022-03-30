b4f6df416c65686bd1ce4a8486f12f7d73900645
Dimarik <krasnodar93@mail.ru>
2022-03-30 15:21:47 +0300

    доработка 3003-2022- Fitness2

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -4,13 +4,14 @@ public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
         while (true) {
+            month++;
             ivan *= 3;
             nik *= 2;
 
             if (ivan > nik) {
                 return month;
             }
-            month++;
+
         }
     }
 }
