a263b5377a3617729de6af08ec2ae27e7668429e
Dimarik <krasnodar93@mail.ru>
2022-03-31 10:31:02 +0300

    доработка 31-03-2022- CheckPrimeNumber

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -4,6 +4,10 @@ public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = true;
 
+        if (number < 2) {
+            return false;
+        }
+
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 prime = false;
