12f2404246850c823a8dd8b2fd9eb8880c90cde9
Dimarik <krasnodar93@mail.ru>
2022-03-31 10:20:14 +0300

    доработка 31-03-2022- простые числа primenumber test

--- src/test/java/ru/job4j/loop/PrimeNumberTest.java
+++ src/test/java/ru/job4j/loop/PrimeNumberTest.java
@@ -14,4 +14,28 @@ public class PrimeNumberTest {
         int result = PrimeNumber.calc(num);
         Assert.assertEquals(result, expected);
     }
+
+    @Test
+    public void when5() {
+        int finish = 5;
+        int result = PrimeNumber.calc(finish);
+        int expected = 3;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void when11() {
+        int finish = 11;
+        int result = PrimeNumber.calc(finish);
+        int expected = 5;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void when2() {
+        int finish = 2;
+        int result = PrimeNumber.calc(finish);
+        int expected = 1;
+        Assert.assertEquals(expected, result);
+    }
 }
