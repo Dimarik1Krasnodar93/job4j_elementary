302e9cf917062fdcac41ee193f8a6b3f817b43f6
Dimarik <krasnodar93@mail.ru>
2022-03-31 10:28:16 +0300

    доработка 31-03-2022- mortgage

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double duty = amount;
-        while (duty > 0) {
-            duty *= (100 + percent) * 0.01;
-            duty -= salary;
+        while (amount > 0) {
+            amount *= (100 + percent) * 0.01;
+            amount -= salary;
             year++;
         }
         return year;
