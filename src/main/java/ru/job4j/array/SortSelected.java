9c2ce2bc476ac4ebdb8d56dde756edd2d71f4c65
Dimarik <krasnodar93@mail.ru>
2022-04-01 12:26:57 +0300

    доработка 01-04-2022- sort 12 26

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -4,8 +4,9 @@ public class SortSelected {
     public static int[] sort(int[] data) {
 
         int[] result = new int[data.length];
-        int index = -1;
+
         for (int i = 0; i < data.length; i++) {
+            int index = -1;
             result[i] = MinDiapason.findMin(data, i, data.length - 1);
             index = FindLoop.indexInRange(data, result[i], i, data.length - 1);
             SwitchArray.swap(data, i, index);
