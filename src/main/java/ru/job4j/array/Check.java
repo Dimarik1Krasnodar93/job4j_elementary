aaf44adc6ddc127c9990cb9e7eb11f94319a2816
Dimarik <krasnodar93@mail.ru>
2022-04-01 10:15:55 +0300

    доработка 01-04-2022- check

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -2,6 +2,7 @@ package ru.job4j.array;
 
 public class Check {
     public static boolean mono(boolean[] data) {
+
         boolean result = true;
         for (int i = 1; i < data.length; i++) {
             if (data[i - 1] != data[i]) {
