55a888b8b84a30cb19fdc1ca726e5742f8d97a88
Dimarik <krasnodar93@mail.ru>
2022-04-02 14:42:16 +0300

    доработка 02-04-2022- SortSelected2

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -2,7 +2,6 @@ package ru.job4j.array;
 
 public class SortSelected {
     public static int[] sort(int[] data) {
-
         int[] result = new int[data.length];
 
         for (int i = 0; i < data.length; i++) {
