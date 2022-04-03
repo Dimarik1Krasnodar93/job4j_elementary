39ee6bb25186a752c0434186b8bdb5ca91068c8d
Dimarik <krasnodar93@mail.ru>
2022-04-03 20:34:19 +0300

    доработка 03-04-2022- song2

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -1,14 +1,6 @@
 package ru.job4j.oop;
 
-public class Student {
-    public void music() {
-        System.out.println("Tra tra tra");
-    }
-
-    public void music(String lyrics) {
-        System.out.println("I can sign a song : " + lyrics);
-    }
-
+public class Jukebox {
     public void music(int position) {
         String song = "";
         switch (position) {
@@ -24,19 +16,11 @@ public class Student {
         System.out.println(song);
     }
 
-    public void song() {
-        System.out.println("I believe I can fly");
-    }
-
     public static void main(String[] args) {
         Student petya = new Student();
-        String song = "I believe, I can fly";
-        petya.music(song);
         petya.music(1);
         petya.music(2);
         petya.music(4);
-        petya.song();
-        petya.song();
-        petya.song();
+
     }
 }
