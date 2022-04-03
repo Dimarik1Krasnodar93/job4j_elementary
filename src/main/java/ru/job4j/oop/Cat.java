ad92bd90ddbe39949e656f95e9eed8362f115fcf
Dimarik <krasnodar93@mail.ru>
2022-04-03 20:55:37 +0300

    доработка 03-04-2022- dummy dic

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -18,6 +18,11 @@ public class Cat {
         this.food = meat;
     }
 
+    public String sound() {
+        String voice = "may-may";
+        return voice;
+    }
+
     public static void main(String[] args) {
         System.out.println("There are gav's food.");
         Cat gav = new Cat();
@@ -29,5 +34,8 @@ public class Cat {
         black.giveNick("Черный");
         black.eat("fish");
         black.show();
+        Cat peppy = new Cat();
+        String say = peppy.sound();
+        System.out.println("Peppy says " + say);
     }
 }
