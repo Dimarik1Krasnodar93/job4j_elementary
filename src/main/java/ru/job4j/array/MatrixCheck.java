113463ea52e96bd3e960bf24c2f70d6bf3581aab
Dimarik <krasnodar93@mail.ru>
2022-04-03 16:59:05 +0300

    доработка 03-04-2022- is win

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -32,6 +32,7 @@ public class MatrixCheck {
     }
 
     public static boolean isWin(char[][] board) {
+
         for (int i = 0; i < board.length; i++) {
             if (board[i][0] == 'X') {
                 if (monoHorizontal(board, i) || monoVertical(board, i)) {
