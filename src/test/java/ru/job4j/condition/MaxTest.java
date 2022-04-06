f018331239f12f5d2121820f2377ea7d05dc200c
Dimarik <krasnodar93@mail.ru>
2022-04-06 11:08:07 +0300

    доработка 06-04-2022- max

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Assert;
 
+import javax.crypto.Mac;
+
 public class MaxTest {
 
     @Test
@@ -34,4 +36,45 @@ public class MaxTest {
         Assert.assertEquals(result, expected);
     }
 
+    @Test
+    public void max123() {
+        int a1 = 1;
+        int a2 = 2;
+        int a3 = 3;
+        int expected = 3;
+        int result = Max.max(a1, a2, a3);
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void max243() {
+        int a1 = 2;
+        int a2 = 4;
+        int a3 = 3;
+        int expected = 4;
+        int result = Max.max(a1, a2, a3);
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void max2435() {
+        int a1 = 2;
+        int a2 = 4;
+        int a3 = 3;
+        int a4 = 5;
+        int expected = 5;
+        int result = Max.max(a1, a2, a3, a4);
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void max9435() {
+        int a1 = 9;
+        int a2 = 4;
+        int a3 = 3;
+        int a4 = 5;
+        int expected = 9;
+        int result = Max.max(a1, a2, a3, a4);
+        Assert.assertEquals(result, expected);
+    }
 }
